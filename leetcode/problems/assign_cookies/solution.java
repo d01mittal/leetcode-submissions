74e5a68bd0309767fc03c99079bdc72311f99dab
Divyanshu Mittal <144702542+d01mittal@users.noreply.github.com>
2024-01-01 08:20:46 +0000

    Sync LeetCode submission - Assign Cookies (java)

---
+++ leetcode/problems/assign_cookies/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0;
+        int ans=0;
+        int j=0;
+        while(i<g.length&&j<s.length){
+            if(g[i]<=s[j]){
+                ans++;
+                i++;
+                j++;
+                continue;
+            }
+            else{
+                j++;
+            }
+        }
+        return ans;
+    }
+}
