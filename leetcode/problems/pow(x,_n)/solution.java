2e6c6f4643c5902ca727ac05e88ced264d7feb90
Divyanshu Mittal <144702542+d01mittal@users.noreply.github.com>
2024-01-01 11:00:00 +0000

    Sync LeetCode submission - Pow(x, n) (java)

--- leetcode/problems/pow(x,_n)/solution.java
+++ leetcode/problems/pow(x,_n)/solution.java
@@ -1,5 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        double ans=1;
+        long pow=n;
+        if(pow<0)
+        pow=-1*pow;
+        while(pow>0){
+            if(pow%2!=0){
+                ans=ans*x;
+                pow=pow-1;
+            }
+            else{
+                x=x*x;
+                pow=pow/2;
+            }
+        }
+        if(n<0)
+        ans=(double)(1.0)/(double)ans;
+        return ans;
     }
 }
