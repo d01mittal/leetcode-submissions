27d60d8051fe15a1067808a1537d0405077f7cdb
Divyanshu Mittal <144702542+d01mittal@users.noreply.github.com>
2024-01-12 05:30:38 +0000

    Sync LeetCode submission - Determine if String Halves Are Alike (java)

---
+++ leetcode/problems/determine_if_string_halves_are_alike/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        String a=s.substring(0,s.length()/2);
+        String b=s.substring(s.length()/2);
+        int cnt1=0,cnt2=0;
+        for(int i=0;i<s.length()/2;i++){
+            if(a.charAt(i)=='a'||a.charAt(i)=='e'||a.charAt(i)=='i'||a.charAt(i)=='o'||a.charAt(i)=='u'||a.charAt(i)=='A'||a.charAt(i)=='E'||a.charAt(i)=='I'||a.charAt(i)=='O'||a.charAt(i)=='U'){
+                cnt1++;
+            }
+        }
+        for(int i=0;i<s.length()/2;i++){
+            if(b.charAt(i)=='a'||b.charAt(i)=='e'||b.charAt(i)=='i'||b.charAt(i)=='o'||b.charAt(i)=='u'||b.charAt(i)=='A'||b.charAt(i)=='E'||b.charAt(i)=='I'||b.charAt(i)=='O'||b.charAt(i)=='U'){
+                cnt2++;
+            }
+        }
+        if(cnt1==cnt2) return true;
+        return false;
+    }
+}
