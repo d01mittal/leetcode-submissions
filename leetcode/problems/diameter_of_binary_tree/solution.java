a1b52493df1a5422e699768a895a8af616e66a57
Divyanshu Mittal <144702542+d01mittal@users.noreply.github.com>
2024-01-21 20:24:29 +0000

    Sync LeetCode submission - Diameter of Binary Tree (java)

---
+++ leetcode/problems/diameter_of_binary_tree/solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int diameter(TreeNode root, int ans[]){
+        if(root==null)return 0;
+        int lh=diameter(root.left,ans);
+        int rh=diameter(root.right,ans);
+        ans[0]=Math.max(ans[0],lh+rh);
+        return 1+Math.max(lh,rh);
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        int ans[]=new int[1];
+        diameter(root,ans);
+        return ans[0];
+    }
+}
