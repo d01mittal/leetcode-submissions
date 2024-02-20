a4cc6ff7b599e34a4fa6beb7859719bf1eecd405
Divyanshu Mittal <144702542+d01mittal@users.noreply.github.com>
2024-02-20 11:33:23 +0000

    Sync LeetCode submission - Missing Number (java)

--- leetcode/problems/missing_number/solution.java
+++ leetcode/problems/missing_number/solution.java
@@ -1,20 +1,21 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
-        // int ans=0;
+        int xor=0;
         // int hash[]=new int[n+1];
+        // int p=0;
         // for(int i=0;i<nums.length;i++){
         //     hash[nums[i]-0]++;
         // }
         // for(int i=0;i<n+1;i++){
         //     if(hash[i]==0){
-        //         ans=i;
-        //         break;
+        //     p=i;
+        //     break;
         //     }
         // }
-        int xor=0;
-        for(int i=0;i<n;i++)
-        xor^=i^nums[i];
+        for(int i=0;i<n;i++){
+            xor^=i^nums[i];
+        }
         return xor^n;
     }
 }
