b4d2d37f050ee63fa4252decf9f20b6857c5001c
Divyanshu Mittal <144702542+d01mittal@users.noreply.github.com>
2024-10-15 04:36:02 +0000

    Sync LeetCode submission Runtime - 8 ms (50.61%), Memory - 45.8 MB (100.00%)

---
+++ leetcode/3195-separate-black-and-white-balls/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long minimumSteps(String s) {
+        long ans=0;
+        int low=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='0') ans=ans+(long)low;
+            else low++;
+        }
+        return ans;
+    }
+}
